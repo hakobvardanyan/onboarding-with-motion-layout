81f861a2b8e153b1f59144b628618ba65238521b
hakobvardanyan <hakvardanyan@gmail.com>
2020-05-01 13:32:53 +0400

    Name refactoring

--- app/src/main/java/com/hakvardanyan/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/hakvardanyan/onboarding/OnBoardingActivity.kt
@@ -3,9 +3,9 @@ package com.hakvardanyan.onboarding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OnBoardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_onboarding)
     }
 }
